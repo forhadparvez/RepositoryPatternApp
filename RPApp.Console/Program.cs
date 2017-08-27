81c48c7c4259b1394a48d92e99562d0834004be6
forhadparvez <forhadparvezfahim@gmail.com>
2017-08-27 15:28:22 +0600

    "Change main Methode"

--- RPApp.Console/Program.cs
+++ RPApp.Console/Program.cs
@@ -7,32 +7,41 @@ namespace RPApp.Console
     {
         static void Main(string[] args)
         {
+
+            string name, regNo;
+
             var studentManager = new StudentManager();
+            System.Console.WriteLine("Enter Studet Name and Reg No:\n");
+            // get input from console
+            System.Console.Write("Name: ");
+            name = System.Console.ReadLine();
+            System.Console.Write("Reg No: ");
+            regNo = System.Console.ReadLine();
 
-            var aStudent = new Student();
-            aStudent.Name = "Parvez";
-            aStudent.RegNo = "1235";
-            aStudent.Department = new Department()
-            {
-                Name = "CSE"
-            };
-
-            bool isNameExist = studentManager.IsRegNoExist(aStudent.RegNo);
+            bool isNameExist = studentManager.IsRegNoExist(regNo);
             if (isNameExist)
             {
-                System.Console.WriteLine("This {0} RegNo Alredy Exist", aStudent.RegNo);
+                System.Console.WriteLine("\nThis {0} RegNo Alredy Exist", regNo);
             }
             else
             {
+                var aStudent = new Student();
+                aStudent.Name = name;
+                aStudent.RegNo = regNo;
+                aStudent.DepartmentId = 1;
+                //aStudent.Department = new Department()
+                //{
+                //    Name = "CSE"
+                //};
                 var successCount = studentManager.Add(aStudent);
 
                 if (successCount > 0)
                 {
-                    System.Console.WriteLine("Save Success");
+                    System.Console.WriteLine("\nSave Success");
                 }
                 else
                 {
-                    System.Console.WriteLine("Save Fail");
+                    System.Console.WriteLine("\nSave Fail");
                 }
             }
 
